de8797921a7f36f241e03a4b3ddea9501f76a239
lhp3851 <lhp3851@163.com>
2018-11-21 17:35:40 +0800

    选择器
    速眠选择器多样

---
+++ Swift_Demo/CocoaTouch/UIKit/View/Selector/SelectorConst.swift
@@ -0,0 +1,19 @@
+//
+//  Const.swift
+//  Swift_Demo
+//
+//  Created by sumian on 2018/11/21.
+//  Copyright © 2018 Jerry. All rights reserved.
+//
+
+import Foundation
+
+enum SelectorType : UInt{
+    case education
+    case address
+    case date
+    case time
+    case dateAndTime
+    case weight
+    case stature
+}
---
+++ Swift_Demo/Modules/Bases/BaseVeiwController/KKSelectorViewController.swift
@@ -0,0 +1,22 @@
+//
+//  KKSelectorViewController.swift
+//  Swift_Demo
+//
+//  Created by sumian on 2018/11/21.
+//  Copyright © 2018 Jerry. All rights reserved.
+//
+
+import UIKit
+
+class KKSelectorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    
+   
+
+}
