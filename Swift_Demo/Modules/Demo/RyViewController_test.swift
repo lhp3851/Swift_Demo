d15853f786f378b42efc06d81de40d59e9bc1cf4
AHao <Noverre.d@sumian.tech>
2018-12-27 09:00:25 +0800

    [feature] : 增加 RyPicker + show 文件

---
+++ Swift_Demo/Modules/Demo/RyViewController_test.swift
@@ -0,0 +1,26 @@
+//
+//  RyViewController_test.swift
+//  Swift_Demo
+//
+//  Created by sumianiOS1 on 2018/12/25.
+//  Copyright © 2018年 Jerry. All rights reserved.
+//
+
+import UIKit
+
+class RyViewController_test: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.white
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        ryPickerView.show()
+    }
+
+    lazy var ryPickerView:RyPickerView = {
+        let temp = RyPickerView.recordMedichinePicker
+        return temp
+    }()
+}
